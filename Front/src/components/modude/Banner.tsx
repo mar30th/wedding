c9f32f5fac77f225bb2f95131d84e18e32b81566
Đông <mar30hirai@gmail.com>
2023-10-18 17:20:56 +0700

    update front

--- Front/src/components/modude/Banner.tsx
+++ Front/src/components/modude/Banner.tsx
@@ -13,15 +13,16 @@ const Banner = () => {
           baseUrl + "/public/banner_img/DSC_7764.webp'"
         })] min-h-[500px] static bg-fixed bg-cover bg-no-repeat opacity-50`}
       ></section>
-      <div className="w-3/4 absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2">
-        <div className="self-center">
+      <div className="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2">
+          <div className="hero bg-red-500">
             <h1 className="text-6xl font-extrabold tracking-widest text-center">
-              <span>{t("groomName")}</span>
+              {t("groomName")}
               <br className="lg:hidden" />
               <span className="text-gray-900 "> &amp; </span>
               <br className="lg:hidden" />
-              <span>{t("brideName")}</span>
+              {t("brideName")}
             </h1>
+
           {/* <div className="mt-4 text-xl font-normal">
             <p>We're Getting Married!</p>
             <p>Saturday | 18-11-2023 | 11:00 AM.</p>
