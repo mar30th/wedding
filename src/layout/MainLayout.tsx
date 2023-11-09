f3147506661e3c5acd65c3e88aeaeddc5554f9dd
dongtran <mar30hirai@gmail.com>
2023-11-09 21:35:40 +0700

    update client

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from '../components/Header'
 import { Outlet } from 'react-router-dom'
 import Footer from '../components/Footer'
+import { Analytics } from '@vercel/analytics/react';
 
 const MainLayout = () => {
   return (
@@ -9,6 +10,7 @@ const MainLayout = () => {
         {/* <Header /> */}
         <Outlet />
         <Footer />
+        <Analytics />
     </div>
   )
 }
