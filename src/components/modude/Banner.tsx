5dcb8e08149f72a587d4d0e98da89a747a2896dc
dongtran <mar30hirai@gmail.com>
2023-11-10 22:40:07 +0700

    update font

--- src/components/modude/Banner.tsx
+++ src/components/modude/Banner.tsx
@@ -7,7 +7,7 @@ const Banner = () => {
         <div
           className="h-screen w-full bg-transparent bg-g bg-gradient-to-r from-[#d5c4b591] to-[#ffffffba] flex items-center justify-center">
           <div>
-            <h1 className="w-full text-[100px] lg:text-[120px] text-white font-extrabold tracking-widest text-center font-corinthia">
+            <h1 className="w-full text-[100px] lg:text-[120px] text-white tracking-widest text-center font-corinthia">
               We're getting married!
             </h1>
             <p className="text-[20px] text-white">Thứ bảy | 18-11-2023 | 11:00 AM</p>
